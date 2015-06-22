e0fa7326fd754148ac1123bbf58eebe452b14da9
Tim Otten <totten@civicrm.org>
2015-06-21 20:59:28 -0700

    docker-entrypoint.sh, README.md - Autoconfigure `amp`
    No need for the user go through the `amp` wizard. Use `amp config:set`.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,10 +8,10 @@ if [ ! -d /var/www/civicrm/civicrm-buildkit ]; then
   printf "Initializing civicrm-buildkit.\n"
   cd /var/www/civicrm && git clone https://github.com/civicrm/civicrm-buildkit.git
   cd /var/www/civicrm/civicrm-buildkit && ./bin/civi-download-tools
-  # We have to re-chown entire directory because civicrm-buildkit creates hidden
-  # directories (e.g. ~/.amp).
-  chown -R www-data:www-data /var/www/
 fi
+
+## Configure amp
+sudo -u www-data -H /var/www/civicrm/civicrm-buildkit/bin/amp config:set --mysql_type=dsn --mysql_dsn=mysql://root@localhost --httpd_type=apache24 --perm_type=none
 
 # Ensure that apache is configured to work properly with AMP. We don't do this in the 
 # Docker file because then apache will complain if the directory doesn't exist.
@@ -20,6 +20,9 @@ if [ ! -f /etc/apache2/conf-available/amp.conf ]; then
   echo 'IncludeOptional /var/www/.amp/apache.d/*.conf' > /etc/apache2/conf-available/amp.conf
   /usr/sbin/a2enconf amp
 fi
+
+## Ensure that www-data user has permission to all files (eg /var/www/.amp, /var/www/civicrm/civicrm-buildkit/build/)
+chown -R www-data:www-data /var/www/
 
 # Check for a passed in DOCKER_UID environment variable. If it's there
 # then ensure that the www-data user is set to this UID. That way we can
